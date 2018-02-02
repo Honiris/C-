229082a30a384c27c8ad13150eeb21a28a5337a1
joratora <adhiol73546@gmail.com>
2018-02-02 14:44:57 +0100

    photo

---
+++ TinyPhotoshop/TinyPhotoshop/Basics/Basics.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+
+namespace TinyPhotoshop
+{
+    public static class Basics
+    {
+        public static Color Grey(Color color)
+        {
+	        Color gray = Color.FromArgb((int) Math.Floor(color.R * 0.2126),
+		                                (int) Math.Floor(color.G * 0.7152),
+		                                (int) Math.Floor(color.B * 0.0722));
+	        return gray;
+        }
+
+        public static Color Binarize(Color color)
+        {
+			double moy = ((double)color.R + (double)color.G + (double)color.B) / 3;
+	        return (moy >= 127.5) ? Color.White : Color.Black;
+        }
+        
+        public static Color BinarizeColor(Color color)
+        {
+	        double r = (double)color.R;
+	        double g = (double)color.G;
+	        double b = (double)color.B;
+	        
+	        r = (r >= 127.5) ? 255 : 0;
+	        g = (g >= 127.5) ? 255 : 0;
+	        b = (b >= 127.5) ? 255 : 0;
+
+	        Color bc = Color.FromArgb(r, g, b);
+
+	        return bc;
+        }
+        
+        public static Color Negative(Color color)
+        {
+	        int r = color.R;
+	        int g = color.G;
+	        int b = color.B;
+
+	        r = 255 - r;
+	        g = 255 - g;
+	        b = 255 - b;
+			
+	        Color bc = Color.FromArgb(r, g, b);
+
+	        return bc;
+		}
+        
+        public static Color Tinter(Color color, Color tint, int factor)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+
+        public static Image Apply(Bitmap img, Func<Color, Color> func)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+    }
+}
---
+++ TinyPhotoshop/TinyPhotoshop/Basics/Geometry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Mime;
+using System.Security.Policy;
+
+
+namespace TinyPhotoshop
+{
+    public static class Geometry
+    {      
+        public static Image Resize(Bitmap img, int x, int y)
+        {
+			//FIXME
+			throw new NotImplementedException();
+        }
+        
+        public static Image Shift(Bitmap img, int x, int y)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+
+        public static Image SymmetryHorizontal(Bitmap img)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+        
+        public static Image SymmetryVertical(Bitmap img)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+        
+        public static Image SymmetryPoint(Bitmap img, int x, int y)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+        
+        public static Image RotationLeft(Bitmap img)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+        
+        public static Image RotationRight(Bitmap img)
+        {
+			//FIXME
+			throw new NotImplementedException();
+		}
+    }
+}
