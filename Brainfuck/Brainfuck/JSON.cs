7ed5c5585705624f9a6cc5a50ad169f43f8e4d13
joratora <adhiol73546@gmail.com>
2018-02-13 13:46:15 +0100

    Brainfuck

--- Brainfuck/Brainfuck/JSON.cs
+++ Brainfuck/Brainfuck/JSON.cs
@@ -50,14 +50,36 @@ namespace Brainfuck
 
         public static JSONElement.JSONType GetJsonType(char c)
         {
-            // TODO
-            throw new NotImplementedException();
+            if (c == 'n')
+                return JSONElement.JSONType.NULL;
+            if (c == 't' || c == 'f')
+                return JSONElement.JSONType.BOOL;
+            if ((int) c >= 48 && (int) c <= 57)
+                return JSONElement.JSONType.NB;
+            if (c == '{')
+                return JSONElement.JSONType.DIC;
+            if (c == '[')
+                return JSONElement.JSONType.LIST;
+            if (c == (char) 34)
+                return JSONElement.JSONType.STR;
         }
 
         public static string ParseString(string json, ref int index)
         {
-            // TODO
-            throw new NotImplementedException();
+            int a = index + 1;
+            int b = a;
+
+            if (json[a] == (char) 34)
+            {
+                return "":
+            }
+            else
+            {
+                while (json[b] != (char) 34 && json[b - 1] != (char) 92)
+                    b++;
+                
+                return json.Substring(a, b - 1);
+            }
         }
 
 
