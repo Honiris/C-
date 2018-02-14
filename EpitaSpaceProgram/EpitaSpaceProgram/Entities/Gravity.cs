d86dc9444682a8659105cb80c0e66209169bf562
joratora <adhiol73546@gmail.com>
2018-02-14 15:13:31 +0100

    gravity tp9

--- EpitaSpaceProgram/EpitaSpaceProgram/Entities/Gravity.cs
+++ EpitaSpaceProgram/EpitaSpaceProgram/Entities/Gravity.cs
@@ -1,16 +1,21 @@
-﻿namespace EpitaSpaceProgram
+﻿using System;
+
+namespace EpitaSpaceProgram
 {
     public class Gravity : Body
     {
+        private Vector2 gravity;
+        
         public Gravity(string name, double mass, double density, Vector2 initialPosition, Vector2 gravity)
             : base(name, mass, density, initialPosition)
         {
-            new 
+            this.gravity = gravity;
         }
 
         public override void Update(double delta)
         {
-            // FIXME
+            base.Update(delta);
+            Acceleration = gravity;
         }
     }
 }
--- EpitaSpaceProgram/EpitaSpaceProgram/Entities/Spring.cs
+++ EpitaSpaceProgram/EpitaSpaceProgram/Entities/Spring.cs
@@ -2,6 +2,8 @@
 {
     public class Spring : Body
     {
+        
+        
         public Spring(string name, double mass, double density, Vector2 initialPosition, Vector2 origin, double spring)
             : base(name, mass, density, initialPosition)
         {
