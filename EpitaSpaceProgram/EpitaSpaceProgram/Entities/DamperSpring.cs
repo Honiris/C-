80e34a84f237152820f213d3ad2fc8e85f0f56de
joratora <adhiol73546@gmail.com>
2018-02-14 16:54:22 +0100

    gravity tp9

--- EpitaSpaceProgram/EpitaSpaceProgram/Entities/DamperSpring.cs
+++ EpitaSpaceProgram/EpitaSpaceProgram/Entities/DamperSpring.cs
@@ -2,19 +2,24 @@
 {
     public class DamperSpring : Spring
     {
-        private double dumpling;
+        private double damping;
+        private Vector2 origin;
+        private Vector2 pos;
+        
         
         public DamperSpring(string name, double mass, double density, Vector2 initialPosition, Vector2 origin,
             double spring, double damping)
             : base(name, mass, density, initialPosition, origin, spring)
         {
-            this.dumpling = damping;
+            this.damping = damping;
+            this.origin = origin;
+            pos = initialPosition;
         }
 
         public override void Update(double delta)
         {
             base.Update(delta);
-            Acceleration += -dumpling * ((Position - origin) / delta);
+            Acceleration += -damping * ((Position - origin) / delta);
         }
     }
 }
--- EpitaSpaceProgram/EpitaSpaceProgram/Entities/Spring.cs
+++ EpitaSpaceProgram/EpitaSpaceProgram/Entities/Spring.cs
@@ -5,8 +5,8 @@ namespace EpitaSpaceProgram
 {
     public class Spring : Body
     {
-        protected double spring;
-        protected Vector2 origin;
+        private double spring;
+        private Vector2 origin;
 
         public Spring(string name, double mass, double density, Vector2 initialPosition, Vector2 origin, double spring)
             : base(name, mass, density, initialPosition)
